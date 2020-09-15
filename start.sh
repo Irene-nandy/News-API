de3c4de6e9792354117dbaaf4fa3d90a9fe2b2c1
Irene-nandy <irenekasiva75@gmail.com>
2020-09-15 16:15:12 +0300

    add start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-# export NEWS_API_KEY='3da9679d84c645fa80b1a7845aa12a71'
-# export SECRET_KEY='secret'
+export NEWS_API_KEY='3da9679d84c645fa80b1a7845aa12a71'
+export SECRET_KEY='secret'
 
-# python3.6 manage.py server
+python3.6 manage.py server
