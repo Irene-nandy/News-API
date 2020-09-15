463c0ab2e292c3889cae59f344f193c5e0e33dd3
Irene-nandy <irenekasiva75@gmail.com>
2020-09-15 16:10:33 +0300

    add init.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,25 @@
-# from flask import Flask
-# from flask_bootstrap import Bootstrap
-# from config import config_options
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
 
-# def create_app(config_name):
-#     app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
 
-#     # create the app configurations
-#     app.config.from_object(config_options[config_name])
+    # create the app configurations
+    app.config.from_object(config_options[config_name])
 
-#     # Initializing flask extensions
-#     bootstrap.init_app(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
-#     # Registering the blueprint
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-#     # setting config
-#     from .requests import configure_request
-#     configure_request(app)
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
-#     return app
+    return app
